b4581fef9dc3dce4c2acbef66424474736ca040a
Nerixyz <nerixdev@outlook.de>
2024-01-13 13:22:34 +0100

    fix: double

--- src/main.cpp
+++ src/main.cpp
@@ -179,7 +179,7 @@ TEST(Serialize, SimpleAnyMap)
     EXPECT_TRUE(out.size() == 3);
     EXPECT_TRUE(any_cast<int>(out["a"]) == 1);
     EXPECT_TRUE(any_cast<std::string>(out["b"]) == "asd");
-    EXPECT_TRUE(any_cast<double>(out["c"]) == 3.14);
+    EXPECT_DOUBLE_EQ(any_cast<double>(out["c"]), 3.14);
 }
 
 TEST(Serialize, ComplexAnyMap)
@@ -237,7 +237,7 @@ TEST(Serialize, ComplexAnyMap)
     EXPECT_TRUE(innerMap.size() == 3);
     EXPECT_TRUE(any_cast<int>(innerMap["a"]) == 420);
     EXPECT_TRUE(any_cast<int>(innerMap["b"]) == 320);
-    EXPECT_TRUE(any_cast<double>(innerMap["c"]) == 13.37);
+    EXPECT_DOUBLE_EQ(any_cast<double>(innerMap["c"]), 13.37);
 
     auto innerArray = any_cast<std::vector<std::any>>(out["innerArray"]);
     EXPECT_TRUE(innerArray.size() == 9);
@@ -248,7 +248,7 @@ TEST(Serialize, ComplexAnyMap)
     EXPECT_TRUE(any_cast<std::string>(innerArray[4]) == "testman");
     EXPECT_TRUE(any_cast<bool>(innerArray[5]) == true);
     EXPECT_TRUE(any_cast<bool>(innerArray[6]) == false);
-    EXPECT_TRUE(any_cast<double>(innerArray[7]) == 4.20);
+    EXPECT_DOUBLE_EQ(any_cast<double>(innerArray[7]), 4.20);
 
     auto innerArrayMap =
         any_cast<std::map<std::string, std::any>>(innerArray[8]);
