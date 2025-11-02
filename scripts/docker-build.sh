15ddee01cd0d98b1da4c1fb98e496c67a5a6265a
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-11-02 14:35:44 +0100

    more cmake testing

---
+++ scripts/docker-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+_fedora_versions=("41" "42" "43" "44")
+_build_types=("system" "conan" "submodule")
+# _fedora_versions=("41")
+# _build_types=("submodule")
+
+for _fedora_version in "${_fedora_versions[@]}"; do
+    for _build_type in "${_build_types[@]}"; do
+        docker build -f .docker/Dockerfile.fedora --tag "pajlada-serialize:fedora-${_fedora_version}-${_build_type}" --build-arg BUILD_TYPE="${_build_type}" . &
+    done
+done
+
+wait
